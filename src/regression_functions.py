9f7bc68763fbf27ec52006627dd81a3f5a2f71b1
KonsKons26 <constantinos.c98@gmail.com>
2025-03-31 21:51:40 +0300

    OOP

--- src/regression_functions.py
+++ src/regression_functions.py
@@ -14,6 +14,16 @@ from joblib import dump, load
 import os
 
 
+def regression_metrics(
+        y_pred,
+        y_test
+) -> dict:
+        mse = mean_squared_error(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        return {"MSE": mse, "MAE": mae, "R2": r2}
+
+
 def baseline_train_test(
         model,
         X_train: pd.DataFrame,
@@ -47,8 +57,8 @@ def baseline_train_test(
 
 
 def baseline_wrapper(
-        X: pd.DataFrame,
-        y: pd.Series,
+        dataset: pd.DataFrame = None,
+        target_col: str = None,
         models: list = ["LinearRegression", "ElasticNet", "SVR", "BayesianRidge"],
         random_state: int = 42,
         test_size: float = 0.2,
@@ -66,10 +76,10 @@ def baseline_wrapper(
 
     Parameters
     ----------
-    X : pd.DataFrame
-        Feature matrix.
-    y : pd.Series
-        Target variable.
+    dataset: pd.DataFrame, default=None
+        Dataset matrix.
+    target_col: str, default=None
+        The column containing the target values.
     models : list, default=["LinearRegression", "ElasticNet", "SVR", "BayesianRidge"]
         List of models to train and evaluate.
     random_state : int, default=42
@@ -88,17 +98,13 @@ def baseline_wrapper(
         Training target variable.
     y_test : pd.Series, default=None
         Test target variable.
-    
+
     Returns
     -------
     metrics : dict
         Dictionary containing the mean squared error (MSE) and R-squared (R2) score
         for each model.
     """
-
-    # Check the types of the incoming data
-    assert isinstance(X, pd.DataFrame)
-    assert isinstance(y, pd.Series)
 
     # Check that the data are presplit or if the function will split them
     if (
@@ -114,6 +120,11 @@ def baseline_wrapper(
 
     # Split the data
     if split_data:
+        # Check the types of the incoming data
+        assert isinstance(dataset, pd.DataFrame)
+        # Extract X and y
+        X = dataset.drop(columns=target_col)
+        y = dataset[target_col]
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=test_size, random_state=random_state
         )
@@ -143,22 +154,131 @@ def baseline_wrapper(
             X_test_scaled,
             y_train
         )
-        mse = mean_squared_error(y_test, y_pred)
-        mae = mean_absolute_error(y_test, y_pred)
-        r2 = r2_score(y_test, y_pred)
-        metrics[model_name]["MSE"] = mse
-        metrics[model_name]["MAE"] = mae
-        metrics[model_name]["R2"] = r2
+        metrics[model_name] = regression_metrics(y_pred, y_test)
 
         if save_models:
             if not save_path:
                 raise ValueError(
                     "Please provide a path to save the models."
                 )
-            full_path = os.path.join(
+            dump(model, os.path.join(
                 save_path,
                 f"{model_name}_baseline.joblib"
-            )
-            dump(model, full_path)
+            ))
+            if scale_data:
+                dump(scaler, os.path.join(
+                    save_path,
+                    f"{model_name}_baseline_scaler.joblib"
+                ))
 
     return metrics
+
+
+class Regresssor:
+
+    valid_types = {
+        "LinearRegression",
+        "ElasticNet",
+        "SVR",
+        "BayesianRidge"
+    }
+
+    valid_modes = {
+        "baseline",
+        "feature_selection",
+        "tuning"
+    }
+
+    def __init__(
+            self,
+            model_type: str,
+            dataset: pd.DataFrame,
+            target_col: str,
+            models_dir: str,
+            random_state: int = 42
+        ):
+
+        if model_type not in self.valid_types:
+            raise ValueError(
+                f"Model must be one of the following:\n{self.valid_types}"
+            )
+        self.model_type = model_type
+
+        assert isinstance(dataset, pd.DataFrame), "dataset must be <pd.DataFrame>"
+        assert isinstance(target_col, str), "target_col must be <str>"
+        self.X = dataset.drop(columns=target_col)
+        self.y = dataset[target_col]
+
+        assert isinstance(models_dir, str), "models_dir must be <str>"
+        self.models_dir = models_dir
+
+        assert isinstance(random_state, int), "random_state must be <int>"
+        self.random_state = random_state
+
+
+    def train(
+            self,
+            mode: str,
+            test_size: float = 0.2,
+            scale_data: bool = True,
+            save_model:bool = False
+        ):
+        if mode not in self.valid_modes:
+            raise ValueError(
+                f"Mode must be one of the following:\n{self.valid_modes}"
+            )
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            self.X, self.y, test_size=test_size, random_state=self.random_state
+        )
+
+        if scale_data:
+            scaler = StandardScaler()
+            scaler.fit(X_train)
+            X_train_scaled = scaler.transform(X_train)
+            X_test_scaled = scaler.transform(X_test)
+
+        # Simple baseline model
+        if mode == "baseline":
+                model_dict = {
+                    "LinearRegression": LinearRegression(),
+                    "ElasticNet": ElasticNet(random_state=self.random_state),
+                    "SVR": SVR(),
+                    "BayesianRidge": BayesianRidge()
+                }
+                model = model_dict[self.model_type]
+
+                model.fit(X_train_scaled, y_train)
+                y_pred = model.predict(X_test_scaled)
+
+                metrics = self._regression_metrics(y_pred, y_test)
+
+                if save_model:
+                    dump(model, os.path.join(
+                        self.models_dir,
+                        f"{self.model_type}_{mode}.joblib"
+                    ))
+                    if scale_data:
+                        dump(scaler, os.path.join(
+                            f"{self.model_type}_{mode}_scaler.joblib"
+                        ))
+
+                return metrics
+
+    def evaluate(
+            self,
+            mode: str,
+            evaluation_set: pd.DataFrame,
+            target_col: str
+    ):
+        pass
+
+    def _regression_metrics(
+            self,
+            y_pred: pd.Series,
+            y_test: pd.Series
+    ) -> dict:
+        mse = mean_squared_error(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        return {"MSE": mse, "MAE": mae, "R2": r2}
