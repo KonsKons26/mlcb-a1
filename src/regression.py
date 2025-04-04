1f2aa3a574e50998890421efef8dbbc5994cead7
KonsKons26 <constantinos.c98@gmail.com>
2025-04-04 15:07:14 +0300

    visualized metrics neatly

--- src/regression.py
+++ src/regression.py
@@ -51,7 +51,37 @@ class Regressor:
             feature_selection_dict: dict = {},
             tune_dict: dict = {},
             save_model: bool = True
-        ):
+        ) -> dict:
+        """Trains the model using the specified mode and parameters.
+
+        Based on the model_type and mode, it will call the appropriate private
+        methods, acting basically as a controller.
+        The modes are:
+            - baseline: Train the model without feature selection or tuning.
+            - feature_selection: Perform feature selection and train the model.
+            - tune: Perform hyperparameter tuning and train the model, using the
+            features selected in the feature selection step.
+        The feature selection and tuning dictionaries can be used to pass
+        parameters to the feature selection and tuning methods, respectively; if
+        they are not provided, default values will be used (recommended).
+
+        Parameters
+        ----------
+        mode : str
+            The mode to use for training. Options are "baseline",
+            "feature_selection", or "tune".
+        feature_selection_dict : dict
+            A dictionary containing parameters for feature selection.
+        tune_dict : dict
+            A dictionary containing parameters for tuning.
+        save_model : bool
+            Whether to save the model and scaler after training.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the training metrics.
+        """
 
         model_name = f"{self.model_type}_{mode}"
 
@@ -104,13 +134,39 @@ class Regressor:
             val_df: pd.DataFrame,
             target: str,
             n_bootstrap: int = 1000
-        ):
+        ) -> dict:
+        """Validates the model using the specified mode and parameters.
+
+        Validation is performed using bootstrap sampling, which allows
+        estimating the performance of the model on unseen data. The
+        validation metrics are calculated for each bootstrap sample.
+
+        Parameters
+        ----------
+        model_name : str
+            The name of the model to validate.
+        mode : str
+            The mode to use for validation. Options are "baseline",
+            "feature_selection", or "tune".
+        val_df : pd.DataFrame
+            The validation dataset to use for validation.
+        target : str
+            The target variable to predict.
+        n_bootstrap : int
+            The number of bootstrap samples to use for validation.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the validation metrics.        
+
+        """
 
         model_name = f"{model_name}_{mode}"
 
         X_val = val_df.drop(columns=[target])
         y_val = val_df[target]
-#
+
         model = self._load_model(model_name)
         scaler = self._load_scaler(model_name)
         if mode != "baseline":
@@ -129,35 +185,12 @@ class Regressor:
 
             for metric_name, metric_values in metrics.items():
                 all_metrics[metric_name].extend(metric_values)
-#
-        # if mode == "baseline":
-        #     all_metrics = self._validate_baseline(
-        #         model_name=model_name,
-        #         X_val=X_val,
-        #         y_val=y_val,
-        #         n_bootstrap=n_bootstrap
-        #     )
-
-        # if mode == "feature_selection":
-        #     all_metrics = self._validate_feature_selection(
-        #         model_name=model_name,
-        #         X_val=X_val,
-        #         y_val=y_val,
-        #         n_bootstrap=n_bootstrap
-        #     )
-
-        # if mode == "tune":
-        #     all_metrics = self._validate_tune(
-        #         model_name=model_name,
-        #         X_val=X_val,
-        #         y_val=y_val,
-        #         n_bootstrap=n_bootstrap
-        #     )
 
         return all_metrics
 
 
-    def _train_model_no_tune(self, mode, feature_selection_dict):
+    def _train_model_no_tune(self, mode, feature_selection_dict) -> None:
+        """Trains the model without tuning, controller private method."""
         if mode == "baseline":
             self._train_baseline()
 
@@ -165,7 +198,17 @@ class Regressor:
             self._feature_selection(feature_selection_dict)
 
 
-    def _train_baseline(self):
+    def _train_baseline(self) -> None:
+        """Trains the model without tuning or feature selection.
+
+        This method is used to train the model using the entire dataset
+        without any feature selection or tuning. It splits the dataset into
+        training and testing sets, standardizes the features, and fits
+        the model to the training data. The metrics are calculated on the
+        testing set and stored in the metrics attribute.
+        The model is saved to the models directory.
+        The scaler is also saved to the models directory.
+        """
         X_train, X_test, y_train, y_test = train_test_split(
             self.X,
             self.y,
@@ -193,6 +236,44 @@ class Regressor:
 
 
     def _feature_selection(self, feature_selection_dict):
+        """Performs feature selection and trains the model.
+
+        This method is used to perform feature selection using different
+        methods and train the model using the selected features. The selection
+        of the feature selection method is based on the metrics calculated
+        during cross-validation. The method iterates over different
+        feature selection methods, applies them to the dataset, and
+        calculates the metrics using KFold cross-validation. In essence, the
+        feature selector is chosen based on the method that yields the best
+        performance for a specific model, so this method is NOT model agnostic.
+        After the best feature selector is found, it is used to transform the
+        dataset and train the model. The metrics are calculated on the
+        testing set and stored in the metrics attribute. The model and scaler are
+        saved to the models directory. The features selected are also saved
+        to the models directory.
+
+        The default values are for the feature selection dictionary are:
+            - threshold: 0.1
+            - k: 20
+            - n_folds: 5
+        The feature selection methods used are:
+            - VarianceThreshold: Removes features with variance below the
+            threshold.
+            - SelectKBest-r_regression: Selects the top k features based on
+            the r_regression score function.
+            - SelectKBest-f_regression: Selects the top k features based on
+            the f_regression score function.
+
+        Parameters
+        ----------
+        feature_selection_dict : dict
+            A dictionary containing parameters for feature selection.
+            The keys are:
+                - threshold: The threshold for the VarianceThreshold method.
+                - k: The number of features to select for the SelectKBest method.
+                - n_folds: The number of folds for KFold cross-validation.
+
+        """
         threshold = feature_selection_dict.get("threshold", 0.1)
         k = feature_selection_dict.get("k", 20)
         n_folds = feature_selection_dict.get("n_folds", 5)
@@ -256,7 +337,7 @@ class Regressor:
 
         self.metrics = all_metrics
 
-        # Train model with the best feature selector
+        # Train model with the best feature selector found
         best_method = self._get_top_feature_selection_method()
         selector = selectors[best_method]
         selector.fit(self.X, self.y)
@@ -300,7 +381,22 @@ class Regressor:
         self._save_features(self.model_type)
 
 
-    def _tune_model(self, param_grid, n_folds):
+    def _tune_model(self, param_grid, n_folds) -> None:
+        """Tunes the model using GridSearchCV.
+
+        This method is used to perform hyperparameter tuning using GridSearchCV. It
+        splits the dataset into training and testing sets, standardizes the features,
+        and fits the model to the training data. The metrics are calculated on the
+        testing set and stored in the metrics attribute. The model is saved to the
+        models directory. The scaler is also saved to the models directory.
+
+        Parameters
+        ----------
+        param_grid : dict
+            A dictionary containing the hyperparameters to tune.
+        n_folds : int
+            The number of folds for KFold cross-validation.
+        """
         best_features = self._load_features(self.model_type)
         X = self.X[best_features]
         y = self.y
@@ -348,7 +444,23 @@ class Regressor:
         self.metrics["features"] = best_features
 
 
-    def _train_ElasticNet(self, mode, feature_selection_dict, tune_dict):
+    def _train_ElasticNet(self, mode, feature_selection_dict, tune_dict) -> None:
+        """Trains the ElasticNet model.
+
+        This private method is used to train the ElasticNet model. It calls the
+        appropriate private methods based on the mode. If no tuning dictionary is
+        provided, default values are used, specific for this model type.
+
+        Parameters
+        ----------
+        mode : str
+            The mode to use for training. Options are "baseline",
+            "feature_selection", or "tune".
+        feature_selection_dict : dict
+            A dictionary containing parameters for feature selection.
+        tune_dict : dict
+            A dictionary containing parameters for tuning.
+        """
         grid_size = tune_dict.get("grid_size", 50)
         if mode == "tune":
             if tune_dict.get("param_grid") is None:
@@ -365,7 +477,23 @@ class Regressor:
             self._train_model_no_tune(mode, feature_selection_dict)
 
 
-    def _train_SVR(self, mode, feature_selection_dict, tune_dict):
+    def _train_SVR(self, mode, feature_selection_dict, tune_dict) -> None:
+        """Trains the SVR model.
+
+        This private method is used to train the ElasticNet model. It calls the
+        appropriate private methods based on the mode. If no tuning dictionary is
+        provided, default values are used, specific for this model type.
+
+        Parameters
+        ----------
+        mode : str
+            The mode to use for training. Options are "baseline",
+            "feature_selection", or "tune".
+        feature_selection_dict : dict
+            A dictionary containing parameters for feature selection.
+        tune_dict : dict
+            A dictionary containing parameters for tuning.
+        """
         grid_size = tune_dict.get("grid_size", 10)
         if mode == "tune":
             if tune_dict.get("param_grid") is None:
@@ -386,7 +514,23 @@ class Regressor:
             self._train_model_no_tune(mode, feature_selection_dict)
 
 
-    def _train_BayesianRidge(self, mode, feature_selection_dict, tune_dict):
+    def _train_BayesianRidge(self, mode, feature_selection_dict, tune_dict) -> None:
+        """Trains the BayesianRidge model.
+
+        This private method is used to train the ElasticNet model. It calls the
+        appropriate private methods based on the mode. If no tuning dictionary is
+        provided, default values are used, specific for this model type.
+
+        Parameters
+        ----------
+        mode : str
+            The mode to use for training. Options are "baseline",
+            "feature_selection", or "tune".
+        feature_selection_dict : dict
+            A dictionary containing parameters for feature selection.
+        tune_dict : dict
+            A dictionary containing parameters for tuning.
+        """
         grid_size = tune_dict.get("grid_size", 10)
         if mode == "tune":
             if tune_dict.get("param_grid") is None:
@@ -406,9 +550,21 @@ class Regressor:
             self._train_model_no_tune(mode, feature_selection_dict)
 
 
-    def _get_top_feature_selection_method(self):
-        # Select the method that yields the largest 
-        # mean(R2) / ((mean(RMSE) + mean(MAE)) / 2)
+    def _get_top_feature_selection_method(self) -> str:
+        """Selects the feature selection method that yields the best performance.
+
+        This method calculates the performance of each feature selection
+        method based on the metrics calculated during cross-validation. It
+        returns the name of the feature selection method that yields the
+        best performance.
+    
+        The performance is calculated as the ratio of the mean R2 score
+        to the mean RMSE and MAE scores:
+        performance = mean_R2 / ((mean_RMSE + mean_MAE) / 2)
+
+        The method with the highest performance is selected as the best feature
+        selection method.
+        """
         all_methods = {
             "VarianceThreshold": [],
             "SelectKBest-r_regression": [],
@@ -418,9 +574,11 @@ class Regressor:
             rmse = metrics["RMSE"]
             mae = metrics["MAE"]
             r2 = metrics["R2"]
+
             mean_r2 = sum(r2) / len(r2)
             mean_rmse = sum(rmse) / len(rmse)
             mean_mae = sum(mae) / len(mae)
+
             all_methods[selection_method] = mean_r2 / ((mean_rmse + mean_mae) / 2)
 
         best_method = max(all_methods, key=all_methods.get)
@@ -428,7 +586,29 @@ class Regressor:
         return best_method
 
 
-    def _regression_metrics(self, y_test, y_pred):
+    def _regression_metrics(self, y_test, y_pred) -> dict:
+        """Calculates regression metrics.
+
+        This method calculates the regression metrics for the model
+        predictions. It calculates the RMSE, MAE, and R2 score for the
+        predictions and returns them in a dictionary.
+        The metrics are calculated using the sklearn.metrics module.
+
+        Parameters
+        ----------
+        y_test : array-like
+            The true target values.
+        y_pred : array-like
+            The predicted target values.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the regression metrics. The keys are:
+                - RMSE: The root mean squared error.
+                - MAE: The mean absolute error.
+                - R2: The R2 score.
+        """
         return {
             "RMSE": [root_mean_squared_error(y_test, y_pred)],
             "MAE": [mean_absolute_error(y_test, y_pred)],
@@ -436,36 +616,42 @@ class Regressor:
         }
 
 
-    def _save_model(self, model_name):
+    def _save_model(self, model_name: str):
+        """Saves the model to the models directory."""
         model_name = model_name + ".joblib"
         dump(self.model, os.path.join(self.models_dir, model_name))
 
 
-    def _save_scaler(self, scaler_name):
+    def _save_scaler(self, scaler_name: str):
+        """Saves the scaler to the models directory."""
         scaler_name = scaler_name + "_scaler.joblib"
         dump(self.scaler, os.path.join(self.models_dir, scaler_name))
 
 
-    def _save_features(self, model_name):
+    def _save_features(self, model_name: str):
+        """Saves the features to the models directory."""
         features_name = model_name + "_features.txt"
         with open(os.path.join(self.models_dir, features_name), "w") as f:
             for feature in self.metrics["features"]:
                 f.write(f"{feature}\n")
 
 
-    def _load_model(self, model_name):
+    def _load_model(self, model_name: str) -> object:
+        """Loads the model from the models directory."""
         model_name = model_name + ".joblib"
         model = load(os.path.join(self.models_dir, model_name))
         return model
 
 
-    def _load_scaler(self, scaler_name):
+    def _load_scaler(self, scaler_name: str) -> object:
+        """Loads the scaler from the models directory."""
         scaler_name = scaler_name + "_scaler.joblib"
         scaler = load(os.path.join(self.models_dir, scaler_name))
         return scaler
 
 
-    def _load_features(self, model_name):
+    def _load_features(self, model_name: str) -> list:
+        """Loads the features from the models directory."""
         features_name = f"{model_name}_features.txt"
         with open(os.path.join(self.models_dir, features_name), "r") as f:
             features = f.readlines()
@@ -482,6 +668,40 @@ def pipeline(
         modes: list = ["baseline", "feature_selection", "tune"],
         save_model: bool = True
     ) -> dict:
+    """Hands-off pipeline for the regression models.
+
+    This function runs the entire pipeline for the regression models. It
+    loads the datasets, trains the models, validates them, and saves the
+    models and scalers. The models are trained using different modes:
+    baseline, feature_selection, and tune. The models are saved to the
+    models directory. The scalers are also saved to the models directory.
+
+    Parameters
+    ----------
+    models_dir : str
+        The directory where the models and scalers will be saved.
+    dataset_dir : str
+        The directory where the datasets are located.
+    target_col : str
+        The target variable to predict. Default is "BMI".
+    model_types : list
+        The list of model types to use for training. Default is
+        ["ElasticNet", "SVR", "BayesianRidge"].
+    modes : list
+        The list of modes to use for training. Default is
+        ["baseline", "feature_selection", "tune"].
+    save_model : bool
+        Whether to save the model and scaler after training. Default is
+        True.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the training and validation metrics for
+        each model type and mode. The keys are:
+            - training: A dictionary containing the training metrics.
+            - validation: A dictionary containing the validation metrics.
+    """
 
     print("----------------")
     print("Running pipeline")
--- src/regression_visualizations.py
+++ src/regression_visualizations.py
@@ -6,6 +6,40 @@ import seaborn as sns
 import plotly.graph_objects as go
 
 
+def pretty_print_metrics(metrics: dict) -> str:
+    """Function to tabulate (in a way) the results of the pipeline."""
+    string = ""
+    string += "========================================================\n"
+    for model in ["ElasticNet", "SVR", "BayesianRidge"]:
+        string += "\t\t--------------------\n"
+        string += f"\t\t{model:^20}\n"
+        string += "\t\t--------------------\n"
+
+        for mode in ["baseline", "feature_selection", "tune"]:
+            string += f"{mode}:\n"
+            string += "\ttraining\t\t\tvalidation\n"
+            string += "\t--------\t\t\t----------\n"
+
+            for (ktrain, vtrain), (kval, vval) in zip(
+                metrics["training"][model][mode].items(),
+                metrics["validation"][model][mode].items()
+            ):
+                string += f"\t{ktrain}:\t{np.mean(vtrain):.4f}\t\t\t{kval}:\t{np.mean(vval):.4f}\n"
+
+            if mode == "feature_selection":
+                string += f"\n\tFeature selection method:\n\t\t\t{metrics["training"][model]["feature_selection"]["feature_selection_method"]}\n"
+                string += f"\tNumber of features used:\n\t\t\t{len(metrics["training"][model]["feature_selection"]["features"])}\n"
+
+            if mode == "tune":
+                string += "\n\tBest hyperparameters:\n"
+                for k, v in metrics["training"][model]["tune"]["best_hyperparameters"].items():
+                    string += f"\t\t\t{k}: {v}\n"
+
+            string += "\n"
+        string += "=========================================================\n"
+    return string
+
+
 def plot_metrics(
         metrics: dict[str, dict[str, list | float]],
         plots: str,
