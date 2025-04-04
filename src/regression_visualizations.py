e01832cf9a622c783401a97ee07a4cbc821b1ff1
KonsKons26 <constantinos.c98@gmail.com>
2025-04-04 15:14:44 +0300

    moved plot_metrics to src

--- src/regression_visualizations.py
+++ src/regression_visualizations.py
@@ -41,93 +41,47 @@ def pretty_print_metrics(metrics: dict) -> str:
 
 
 def plot_metrics(
-        metrics: dict[str, dict[str, list | float]],
-        plots: str,
-        figsize: tuple = (18, 6),
-        models: list = ["ElasticNet", "SVR", "BayesianRidge"],
-        metrics_list: list = ["RMSE", "MAE", "R2"],
-        linewidth: float = 2,
-        title_fontsize: int = 16
+        all_metrics,
+        tr_or_val = "validation",
+        models = ["ElasticNet", "SVR", "BayesianRidge"],
+        modes = ["baseline", "feature_selection", "tune"],
+        metrics = ["RMSE", "MAE", "R2"]
     ) -> None:
-    """
-    Plot metrics with customizable line thickness and title sizes.
-    
-    Parameters
-    ----------
-    metrics: dict[str, dict[str, list | float]]
-        The dictionary containing the metrics per model.
-    plots: str
-        Whether to use boxplots or barplots.
-    figsize: tuple, default = (18, 6)
-        The size of the plotted figure.
-    models: list, default = ["ElasticNet", "SVR", "BayesianRidge"]
-        The list of models to plot.
-    metrics_list: list, default = ["RMSE", "MAE", "R2"]
-        The list of metrics to plot.
-    linewidth: float, default = 2
-        The linewidth of the edges of the boxes/ bars.
-    title_fontsize: int, default = 16
-        The fontsize of the title.
 
-    Returns
-    -------
-    None
-    """
-
-    # Check plot called
-    if plots != "boxplot" and plots != "barplot":
-        raise ValueError(
-            "Either set 'plots' to 'boxplot' or 'barplot'."
+    def plot_metrics_helper(
+        model,
+        metric,
+        df
+    ):
+        plt.figure(figsize=(10, 10))
+        sns.boxplot(
+            data=df
         )
+        plt.title(f"{model} - {metric}")
+        plt.show()
 
-    # Convert the metrics dictionary from {model: {metric: value | values}} to
-    # {metric: {model: value | values}} to plot based on metric
-    metrics_by_metric = {
-        metric: {model: [] for model in models} for metric in metrics_list
+    
+    metrics_dict = {
+        model: {
+            metric: {
+                mode: [] for mode in modes
+            } for metric in metrics
+        } for model in models
     }
-    for model, metrics_dict in metrics.items():
-        if model in models:
-            for metric, values_list in metrics_dict.items():
-                if metric in metrics_list:
-                    metrics_by_metric[metric][model] = values_list
 
-    # Plotting
-    _, axes = plt.subplots(nrows=1, ncols=3, figsize=figsize)
+    for model in models:
+        for mode in modes:
+            for metric in metrics:
+                metrics_dict[model][metric][mode].extend(all_metrics[tr_or_val][model][mode][metric])
 
-    for i, ax in enumerate(axes):
-        metric = list(metrics_by_metric.keys())[i]
-        values = metrics_by_metric[metric]
 
-        if plots == "boxplot":
-            # Boxplot with thicker lines
-            boxplot = sns.boxplot(
-                data=values,
-                ax=ax,
-                color="#2a59a3",
-                linewidth=linewidth
+    for model in models:
+        for metric in metrics:
+            df = pd.DataFrame(columns=modes)
+            for mode in modes:
+                df[mode] = metrics_dict[model][metric][mode]
+            plot_metrics_helper(
+                model,
+                metric,
+                df
             )
-            # Make median line thicker
-            for artist in boxplot.artists:
-                artist.set_edgecolor('black')
-                artist.set_linewidth(linewidth)
-        elif plots == "barplot":
-            # Barplot with thicker lines
-            barplot = sns.barplot(
-                data=values,
-                ax=ax,
-                color="#2a59a3",
-                linewidth=linewidth
-            )
-            # Make outline thicker
-            for patch in barplot.patches:
-                patch.set_edgecolor('black')
-                patch.set_linewidth(linewidth)
-
-        ax.set_title(metric, fontsize=title_fontsize)
-
-        # Make axis lines thicker
-        for spine in ax.spines.values():
-            spine.set_linewidth(linewidth/2)
-
-    plt.tight_layout()
-    plt.show()
